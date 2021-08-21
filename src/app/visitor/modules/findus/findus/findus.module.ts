b32eaebea475e7dc0d7b5e5e62299f6ee875cbe3
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-08-21 04:16:54 -0400

    feat(visitor/modules): findus module added

---
+++ src/app/visitor/modules/findus/findus/findus.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FindusRoutingModule } from './findus-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FindusRoutingModule
+  ]
+})
+export class FindusModule { }
