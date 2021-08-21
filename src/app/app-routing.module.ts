b9f1f3cc5a740dee56c650d671af1320ad8e1f71
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-08-21 04:18:28 -0400

    feat(app): routing imported

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
+import { HomeComponent } from './visitor/screens/home/home.component';
+import { UserGuard } from './core/guards/user/user.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminGuard } from '@core/guards/admin/admin.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'visitor/inicio',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'visitor',
+    loadChildren: () => import('./visitor/visitor.module')
+      .then(VisitorModule => VisitorModule.VisitorModule)
+  },
+  {
+    path: 'user',
+    canActivate: [UserGuard],
+    loadChildren: () => import('./user/user.module')
+      .then(m => m.UserModule)
+  },
+  {
+    path: 'admin',
+    canActivate: [AdminGuard],
+    loadChildren: () => import('./admin/admin.module')
+      .then(AdminModule => AdminModule.AdminModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'visitor/inicio',
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
