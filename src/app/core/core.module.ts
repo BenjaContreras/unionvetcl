01c13483f24cf8cd9a75d7f7eb7ecf078ee4e957
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-08-21 04:15:22 -0400

    feat(app): core module added

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpService } from './services/http/http.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+const services = [
+  HttpService,
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [...services]
+})
+export class CoreModule { }
