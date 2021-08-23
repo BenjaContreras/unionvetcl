33e2d7092065635bb22e31a02dd8af4f84d10f6a
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-08-22 22:07:03 -0400

    feat(visitor): module fixed

--- src/app/visitor/visitor.module.ts
+++ src/app/visitor/visitor.module.ts
@@ -3,19 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { VisitorRoutingModule } from './visitor-routing.module';
 import { HomeComponent } from './screens/home/home.component';
-import { LoginComponent } from './screens/login/login.component';
-import { RegisterComponent } from './screens/register/register.component';
-import { FindusComponent } from './screens/findus/findus.component';
-import { ContactusComponent } from './screens/contactus/contactus.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    FindusComponent,
-    ContactusComponent
+    HomeComponent
   ],
   imports: [
     CommonModule,
