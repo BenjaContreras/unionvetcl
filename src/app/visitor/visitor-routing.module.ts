242f0582a8b0c51205373d6930c0696e7e81a482
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-14 12:03:03 -0300

    feat(visitor): lazy load included

--- src/app/visitor/visitor-routing.module.ts
+++ src/app/visitor/visitor-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./modules/home/home.module')
+          .then(hM => hM.HomeModule)
+      },
+      {
+        path: 'servicios',
+        loadChildren: () => import('./modules/services/services.module')
+          .then(sM => sM.ServicesModule)
+      },
+      {
+        path: 'productos',
+        loadChildren: () => import('./modules/products/products.module')
+          .then(pM => pM.ProductsModule)
+      },
+      {
+        path: 'contactos',
+        loadChildren: () => import('./modules/contactus/contactus.module')
+          .then(cM => cM.ContactusModule)
+      },
+    ],
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./modules/home/home.module')
+      .then(hM => hM.HomeModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
