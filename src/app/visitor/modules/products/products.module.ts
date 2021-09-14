174607c5e003616c7b34e6baf7438272f4ab68ef
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-14 12:01:54 -0300

    feat(visitor/products): module added

---
+++ src/app/visitor/modules/products/products.module.ts
@@ -0,0 +1,14 @@
+import { ProductsRoutingModule } from './products-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule
+  ]
+})
+export class ProductsModule { }
