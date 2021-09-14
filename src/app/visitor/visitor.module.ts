be13773d170c208de11346c667d032aa1da59ef0
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-14 12:03:43 -0300

    chore(visitor): home screen removed into a module

--- src/app/visitor/visitor.module.ts
+++ src/app/visitor/visitor.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VisitorRoutingModule } from './visitor-routing.module';
-import { HomeComponent } from './screens/home/home.component';
 
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     VisitorRoutingModule
