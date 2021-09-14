7c1c0d6d0ac1bd0b16707a12b2fce2e9a70802d1
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-14 12:02:25 -0300

    feat(visitor/services): module added

---
+++ src/app/visitor/modules/services/services.module.ts
@@ -0,0 +1,14 @@
+import { ServicesRoutingModule } from './services-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ServicesRoutingModule
+  ]
+})
+export class ServicesModule { }
