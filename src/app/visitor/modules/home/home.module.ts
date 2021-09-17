f0f32e93af4a53299289f0e32279a640fd14a2ac
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-17 05:37:03 -0300

    chore(visitor/home): new component added

--- src/app/visitor/modules/home/home.module.ts
+++ src/app/visitor/modules/home/home.module.ts
@@ -5,12 +5,14 @@ import { LandingComponent } from './screens/landing/landing.component';
 import { SharedModule } from '@shared/shared.module';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
-import { ServicesComponent } from './components/services/services.component';
 import { MaterialModule } from '@core/material.module';
+import { TipsComponent } from './components/tips/tips.component';
+import { TipCardDetailComponent } from './components/tip-card-detail/tip-card-detail.component';
 
 const components: any = [
   LandingComponent, CarrouselComponent,
-  AboutusComponent, ServicesComponent
+  AboutusComponent, TipsComponent,
+  TipCardDetailComponent
 ];
 @NgModule({
   declarations: [...components],
