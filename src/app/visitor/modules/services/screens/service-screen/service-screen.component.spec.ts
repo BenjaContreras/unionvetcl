7d1af36e5849c3dcf2b6f8a5221a606e716c088c
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-19 20:34:57 -0300

    feat(visitor/services): srvices screen added

---
+++ src/app/visitor/modules/services/screens/service-screen/service-screen.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServiceScreenComponent } from './service-screen.component';
+
+describe('ServiceScreenComponent', () => {
+  let component: ServiceScreenComponent;
+  let fixture: ComponentFixture<ServiceScreenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ServiceScreenComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServiceScreenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/visitor/modules/services/screens/service-screen/service-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-screen',
+  templateUrl: './service-screen.component.html',
+  styleUrls: ['./service-screen.component.sass']
+})
+export class ServiceScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
