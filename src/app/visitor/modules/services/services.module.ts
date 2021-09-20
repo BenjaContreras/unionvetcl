ca7149cc59347b0a9e80d4277790a6ad2309e0e6
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-20 01:40:55 -0300

    feat(visitor/services): new modules included

--- src/app/visitor/modules/services/services.module.ts
+++ src/app/visitor/modules/services/services.module.ts
@@ -1,15 +1,21 @@
 import { ServicesRoutingModule } from './services-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ServiceScreenComponent } from './screens/service-screen/service-screen.component';
+import { SharedModule } from '@shared/shared.module';
 import { SwiperModule } from 'swiper/angular';
 
-
-
+const components: any = [
+  ServiceScreenComponent,
+]
 @NgModule({
-  declarations: [],
+  declarations: [...components],
   imports: [
     CommonModule,
     ServicesRoutingModule,
+    SharedModule,
     SwiperModule
+  ],
+  exports: [...components]
 })
 export class ServicesModule { }
