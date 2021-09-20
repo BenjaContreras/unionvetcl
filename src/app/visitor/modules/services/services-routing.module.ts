5e80f8de28f13d6b937230dd85c32164663c58e4
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-20 01:40:32 -0300

    feat(visitor/services): routing redirect

--- src/app/visitor/modules/services/services-routing.module.ts
+++ src/app/visitor/modules/services/services-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiceScreenComponent } from './screens/service-screen/service-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ServiceScreenComponent, pathMatch: 'full' },
+  { path: '**', component: ServiceScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
