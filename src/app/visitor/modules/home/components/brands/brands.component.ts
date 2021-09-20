11c94ba1d9ef1dd6ebe554e2dd33e291bc756fc0
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-20 00:10:12 -0300

    feat(visitor/brands): brand component created

---
+++ src/app/visitor/modules/home/components/brands/brands.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-brands',
+  templateUrl: './brands.component.html',
+  styleUrls: ['./brands.component.sass']
+})
+export class BrandsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
