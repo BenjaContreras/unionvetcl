cbab72bf3edf2473d59a125e77284894639a0f7d
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-21 15:17:32 -0300

    feat(shared): component scroll added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MaterialModule } from '@core/material.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ScrollTopButtonComponent } from './components/scroll-top-button/scroll-top-button.component';
 
 const components: any = [
   NavbarComponent, FooterComponent,
+  ScrollTopButtonComponent
 ];
 
 @NgModule({
