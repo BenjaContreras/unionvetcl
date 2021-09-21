fdeb589cef9fceb80b6f6405144c483b18ffdd89
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-21 15:17:16 -0300

    feat(shared): new component for scroll button included

--- src/app/shared/components/scroll-top-button/scroll-top-button.component.ts
+++ src/app/shared/components/scroll-top-button/scroll-top-button.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit, OnChanges } from '@angular/core';
 
 @Component({
-  selector: 'app-scroll-top-button',
+  selector: 'scroll-top-button',
   templateUrl: './scroll-top-button.component.html',
   styleUrls: ['./scroll-top-button.component.sass']
 })
-export class ScrollTopButtonComponent implements OnInit {
+export class ScrollTopButtonComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  public windowScrolled: boolean;
+  public yZone: string;
 
-  ngOnInit(): void {
+  constructor(@Inject(DOCUMENT) private document: Document) { 
+    this.windowScrolled = false;
+    this.yZone = this.onWindowScroll();
   }
 
+  ngOnInit(): void {
+    this.onWindowScroll();
+  }
+
+  ngOnChanges(): void {
+    this.yZone = this.onWindowScroll();
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) this.windowScrolled = true;
+    else {
+      if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) this.windowScrolled = false;
+    }
+    if (document.documentElement.scrollTop > 2180) return this.yZone = 'white';
+    else return this.yZone = '#0F7499';
+  }
+  scrollToTop() {
+    (function smoothscroll() {
+        var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        if (currentScroll > 0) {
+            window.scrollTo(0, 0);
+        }
+    })();
+  }
 }
