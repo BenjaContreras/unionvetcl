2bf1dbe43183ab37ec9a9e6a024f3b4e155018b5
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-29 19:36:48 -0300

    feat(visitor/services): input for service included

--- src/app/visitor/modules/services/components/services-detail/services-detail.component.ts
+++ src/app/visitor/modules/services/components/services-detail/services-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Service } from '@models/service.model';
 
 @Component({
   selector: 'services-detail',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() public service: Service | null;
+    this.service = null;
 
   ngOnInit(): void {
   }
