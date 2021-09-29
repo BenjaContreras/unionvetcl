72abb1450755982b4a18d2b4a932ac88378f1af0
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-09-29 19:38:14 -0300

    feat(visitor/services): variable paragraph included for the content

--- src/app/visitor/modules/services/components/services-detail/services-detail.component.ts
+++ src/app/visitor/modules/services/components/services-detail/services-detail.component.ts
@@ -9,11 +9,18 @@ import { Service } from '@models/service.model';
 export class ServicesDetailComponent implements OnInit, OnChanges {
 
   @Input() public service: Service | null;
+  public paragraph: string[];
+
+  constructor() { 
     this.service = null;
+    this.paragraph = [];
+  }
 
   ngOnInit(): void {
   }
 
   ngOnChanges(): void {
+    this.paragraph = [];
+    this.service?.content.forEach(text => this.paragraph.push(text));
   }
 }
