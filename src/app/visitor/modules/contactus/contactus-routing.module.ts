f77d6cc2f9a7b92fbcceec42514fcd5a948df071
Hectorveas <hveas10@gmail.com>
2021-10-07 01:44:02 -0300

    feat(visitor/contactus): form added

--- src/app/visitor/modules/contactus/contactus-routing.module.ts
+++ src/app/visitor/modules/contactus/contactus-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactusComponent } from './contactus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContactusComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/visitor/modules/contactus/contactus.module.ts
+++ src/app/visitor/modules/contactus/contactus.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactusRoutingModule } from './contactus-routing.module';
+import { ContactusComponent } from './contactus.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactusComponent
+  ],
   imports: [
     CommonModule,
-    ContactusRoutingModule
+    ContactusRoutingModule,
+    SharedModule
   ]
 })
 export class ContactusModule { }
