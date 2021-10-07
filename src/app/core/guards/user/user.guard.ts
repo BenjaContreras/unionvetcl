8cd5f78fda7cdb3511319f9f6629b6fb8e70e712
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-07 00:55:11 -0300

    feat(core/guards): userGuard included

--- src/app/core/guards/user/user.guard.ts
+++ src/app/core/guards/user/user.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthProviderService } from '@core/providers/auth/auth-provider.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
+
+  constructor(
+    private authProvider: AuthProviderService,
+    private router: Router
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if (this.authProvider.isAuthenticated('user')) {
+        return true;
+      } else {
+        this.router.navigate(['visitor/inicio']);
+        return false;
+      }
   }
-  
 }
