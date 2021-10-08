4f9b9c4ee15c7f2e90166ac6e949a8664c61e07b
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-08 02:57:28 -0300

    feat(visitor/products): sidebar of the filters

---
+++ src/app/visitor/modules/products/components/side-filters/side-filters/side-filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'products-side-filters',
+  templateUrl: './side-filters.component.html',
+  styleUrls: ['./side-filters.component.sass']
+})
+export class SideFiltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
