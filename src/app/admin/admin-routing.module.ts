7339d8285382876354b697b78cbe07ac0ae01104
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-09 19:05:31 -0300

    feat(admin): childrens included in routing

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./modules/dashboard/dashboard.module')
+          .then(dM => dM.DashboardModule)
+      },
+      {
+        path: 'agenda',
+        loadChildren: () => import('./modules/dates/dates.module')
+          .then(dM => dM.DatesModule)
+      },
+      {
+        path: 'usuarios',
+        loadChildren: () => import('./modules/users/users.module')
+          .then(uM => uM.UsersModule)
+      },
+      {
+        path: 'productos',
+        loadChildren: () => import('./modules/products/products.module')
+          .then(pM => pM.ProductsModule)
+      },
+      {
+        path: 'consultas',
+        loadChildren: () => import('./modules/contacts/contacts.module')
+          .then(cM => cM.ContactsModule)
+      },
+      {
+        path: 'publicaciones',
+        loadChildren: () => import('./modules/publications/publications.module')
+          .then(pM => pM.PublicationsModule)
+      },
+    ]
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./modules/dashboard/dashboard.module')
+      .then(dM => dM.DashboardModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
