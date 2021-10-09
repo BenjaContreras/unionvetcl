e392de6c3827dc7de1f59c83ad3f80fea0019c74
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-09 19:36:15 -0300

    feat(admin/publications): screen when the admin come into the module

--- src/app/admin/modules/publications/publications.module.ts
+++ src/app/admin/modules/publications/publications.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicationsRoutingModule } from './publications-routing.module';
+import { RedirectScreenComponent } from './screens/redirect-screen/redirect-screen.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RedirectScreenComponent
+  ],
   imports: [
     CommonModule,
     PublicationsRoutingModule
