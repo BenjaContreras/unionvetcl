bff5fa5f3e5073c178db0b5214c71384b9fa24d6
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-09 19:35:52 -0300

    feat(admin/publications): children routes added

--- src/app/admin/modules/publications/publications-routing.module.ts
+++ src/app/admin/modules/publications/publications-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RedirectScreenComponent } from './screens/redirect-screen/redirect-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: RedirectScreenComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'tips',
+        loadChildren: () => import('./modules/tips/tips.module')
+          .then(tM => tM.TipsModule)
+      },
+      {
+        path: 'redes-sociales',
+        loadChildren: () => import('./modules/social-media/social-media.module')
+          .then(rsM => rsM.SocialMediaModule)
+      }
+    ]
+  },
+  { path: '**', component: RedirectScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
