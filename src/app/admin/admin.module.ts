51406f833ca12bcfe3784546ad1724c397852770
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-09 19:05:46 -0300

    chore(admin): screens deleted

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
 import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
-  declarations: [
-    HomeScreenComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     AdminRoutingModule,
