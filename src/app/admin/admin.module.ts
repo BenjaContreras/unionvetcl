048e8ccd67bb95af8df0efbcdddd1018c9726ecf
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-10 00:43:48 -0300

    feat(admin): new components & modules

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { SharedModule } from '@shared/shared.module';
+import { AdminHomeScreenComponent } from './screens/admin-home-screen/admin-home-screen.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MaterialModule } from '@core/material.module';
+import { AuthProviderService } from '@core/providers/auth/auth-provider.service';
 
-
+const components: any = [
+  AdminHomeScreenComponent
+]
 @NgModule({
-  declarations: [],
+  declarations: [...components],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    SharedModule
+    SharedModule,
+    LayoutModule,
+    MaterialModule
   ]
 })
 export class AdminModule { }
