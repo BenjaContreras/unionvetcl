4d2ae26b49505161201c77fcf32fbaea2e7809b7
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-10 00:42:42 -0300

    feat(admin/dash): route for the screen added

--- src/app/admin/modules/dashboard/dashboard-routing.module.ts
+++ src/app/admin/modules/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalScreenComponent } from './screens/principal-screen/principal-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PrincipalScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
