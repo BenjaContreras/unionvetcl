4388b7c662f4c0e07da681c37ad29521457b5869
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-10 00:42:19 -0300

    feat(admin/dash): components & screens added

--- src/app/admin/modules/dashboard/dashboard.module.ts
+++ src/app/admin/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { PrincipalScreenComponent } from './screens/principal-screen/principal-screen.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { WeekDatesComponent } from './components/week-dates/week-dates.component';
+import { WeekSellsComponent } from './components/week-sells/week-sells.component';
 
-
+const components: any = [
+  PrincipalScreenComponent, ProductsListComponent,
+  WeekDatesComponent, WeekSellsComponent
+];
 @NgModule({
-  declarations: [],
+  declarations: [...components],
   imports: [
     CommonModule,
     DashboardRoutingModule
