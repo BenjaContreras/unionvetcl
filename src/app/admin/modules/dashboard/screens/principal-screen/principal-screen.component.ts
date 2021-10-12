6105029d40335d5b213dd8d7b0b38dc1fc0e570c
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-11 23:25:55 -0300

    feat(admin/dash): screen fixed

--- src/app/admin/modules/dashboard/screens/principal-screen/principal-screen.component.ts
+++ src/app/admin/modules/dashboard/screens/principal-screen/principal-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-principal-screen',
@@ -12,4 +12,8 @@ export class PrincipalScreenComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?: any) {
+    return window.innerWidth;
+  }
 }
