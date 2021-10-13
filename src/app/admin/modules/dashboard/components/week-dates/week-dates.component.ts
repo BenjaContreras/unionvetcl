42d01323be7d861006515c82f6ae3c956cd9c25a
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-13 16:18:56 -0300

    feat(admin/dash): new functions to manipulate the time

--- src/app/admin/modules/dashboard/components/week-dates/week-dates.component.ts
+++ src/app/admin/modules/dashboard/components/week-dates/week-dates.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
@@ -16,8 +16,12 @@ export class WeekDatesComponent implements OnInit {
   public chartLegends: boolean;
   public chartType: ChartType;
   public chartColors: any;
+  public fechaInicio: any;
+  public fechaTermino: any;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
+    const day: Date = new Date();
+    this.setDate(day);
     this.chartData = [
       {
         data: [10, 4, 15, 13, 2],
@@ -49,6 +53,110 @@ export class WeekDatesComponent implements OnInit {
   public goTo(route: string) {
     this.router.navigate([`admin/${route}`]);
   };
+
+  public setDate(day: Date) {
+    let dia = day.getDay();
+    let numero = day.getDate();
+    let mes = day.getMonth();
+    if (dia <= 5) {
+      if (dia === 1) {
+        this.fechaInicio = {
+          dia: this.getDia(dia, numero),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+        this.fechaTermino = {
+          dia: this.getDia(dia + 4, numero + 4),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+      }
+      if (dia === 2) {
+        this.fechaInicio = {
+          dia: this.getDia(dia, numero),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+        this.fechaTermino = {
+          dia: this.getDia(dia + 3, numero + 3),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+      }
+      if (dia === 3) {
+        this.fechaInicio = {
+          dia: this.getDia(dia, numero),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+        this.fechaTermino = {
+          dia: this.getDia(dia + 2, numero + 2),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+      }
+      if (dia === 4) {
+        this.fechaInicio = {
+          dia: this.getDia(dia, numero),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+        this.fechaTermino = {
+          dia: this.getDia(dia + 1, numero + 1),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+      }
+      if (dia === 5) {
+        this.fechaInicio = {
+          dia: this.getDia(dia, numero),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+        this.fechaTermino = {
+          dia: this.getDia(dia, numero),
+          mes: this.getMes(mes),
+          anio: new Date().getFullYear()
+        };
+      }
+    } else {
+      if (day.getDay() === 6) {
+        //
+      }
+      if (day.getDay() === 7) {
+        //
+      }
+    };
+  }
+
+  public getDia(dia: number, numero: number): string | null {
+    switch (dia) {
+      case 1: return `Lunes ${numero}`;
+      case 2: return `Martes ${numero}`;
+      case 3: return `Miercoles ${numero}`;
+      case 4: return `Jueves ${numero}`;
+      case 5: return `Viernes ${numero}`;
+    };
+    return null;
+  };
+
+  public getMes(mes: number): string | null {
+    switch (mes) {
+      case 0: return 'Enero';
+      case 1: return 'Febrero';
+      case 2: return 'Marzo';
+      case 3: return 'Abril';
+      case 4: return 'Mayo';
+      case 5: return 'Junio';
+      case 6: return 'Julio';
+      case 7: return 'Agosto';
+      case 8: return 'Septiembre';
+      case 9: return 'Octubre';
+      case 10: return 'Noviembre';
+      case 11: return 'Diciembre';
+    };
+    return null;
+  }
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?: any) {
