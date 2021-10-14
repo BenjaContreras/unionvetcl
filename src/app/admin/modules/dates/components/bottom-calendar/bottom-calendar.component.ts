46067dba42d1a1a21dd5e4f44768e5b46f5cd927
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-14 15:27:05 -0300

    feat(admin/dates): bottom component added

---
+++ src/app/admin/modules/dates/components/bottom-calendar/bottom-calendar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-bottom-calendar',
+  templateUrl: './bottom-calendar.component.html',
+  styleUrls: ['./bottom-calendar.component.sass']
+})
+export class BottomCalendarComponent implements OnInit {
+
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
+
+}
