8fae5fd9df4b980a26c959fa2a1793918b150bff
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-14 15:28:26 -0300

    test(admin/dates): dummy data included

--- src/app/admin/modules/dates/components/bottom-calendar/bottom-calendar.component.ts
+++ src/app/admin/modules/dates/components/bottom-calendar/bottom-calendar.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BottomCalendarComponent implements OnInit {
 
+  public horasTarde: any[];
 
   constructor() {
-   }
+    this.horasTarde = [
+      {
+        hora: { inicio: new Date("2021-10-13T15:00:00"), fin: new Date("2021-10-13T15:30:00") },
+        pL: 'Benjamin Contreras',
+        pMa: 'Hector Veas',
+        pMi: 'Manuel Urra',
+        pJ: 'Carolina',
+        pV: 'Huaso isla'
+      },
+      { hora: { inicio: new Date("2021-10-13T15:30:00"), fin:  new Date("2021-10-13T16:00:00")}, pL: 'Hector Veas', pMa: 'Pedro Aguilera', pMi: 'Manuel Santelices', pJ: 'Byron Guerrero', pV: 'Renata Prado'},
+      { hora: { inicio: new Date("2021-10-13T16:00:00"), fin:  new Date("2021-10-13T16:30:00")}, pL: 'Felipe Concha', pMa: 'Benjamin Castillo', pMi: 'Cristian Reyes', pJ: 'David Godoy', pV: 'Valentina Marut'},
+      { hora: { inicio: new Date("2021-10-13T16:30:00"), fin:  new Date("2021-10-13T17:00:00")}, pL: 'Hector Veas', pMa: 'Pedro Aguilera', pMi: 'Manuel Santelices', pJ: 'Byron Guerrero', pV: 'Renata Prado'},
+      { hora: { inicio: new Date("2021-10-13T17:00:00"), fin:  new Date("2021-10-13T17:30:00")}, pL: 'Felipe Concha', pMa: 'Benjamin Castillo', pMi: 'Cristian Reyes', pJ: 'David Godoy', pV: 'Valentina Marut'},
+      { hora: { inicio: new Date("2021-10-13T17:30:00"), fin:  new Date("2021-10-13T18:00:00")}, pL: 'Hector Veas', pMa: 'Pedro Aguilera', pMi: 'Manuel Santelices', pJ: 'Byron Guerrero', pV: 'Renata Prado'},
+      { hora: { inicio: new Date("2021-10-13T18:00:00"), fin: new Date("2021-10-13T18:30:00") }, pL: 'Felipe Concha', pMa: 'Benjamin Castillo', pMi: 'Cristian Reyes', pJ: 'David Godoy', pV: 'Valentina Marut' },
+      { hora: { inicio: new Date("2021-10-13T18:30:00"), fin: new Date("2021-10-13T19:00:00") }, pL: 'Felipe Concha', pMa: 'Benjamin Castillo', pMi: 'Cristian Reyes', pJ: 'David Godoy', pV: 'Valentina Marut' },
+    ];
+  }
 
   ngOnInit(): void {
   }
