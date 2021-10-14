f47dec72be37891efde849b9c1c4f9dcf5abaaa4
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-14 15:41:40 -0300

    feat(admin/dates): initialized the month

--- src/app/admin/modules/dates/screens/date-screen/date-screen.component.ts
+++ src/app/admin/modules/dates/screens/date-screen/date-screen.component.ts
@@ -35,14 +35,44 @@ export class FiveDayRangeSelectionStrategy<D> implements MatDateRangeSelectionSt
   styleUrls: ['./date-screen.component.sass']
 })
 export class DateScreenComponent implements OnInit {
+
+  public mes!: string;
+  public range: FormGroup;
+
   constructor() {
     this.range = new FormGroup({
       start: new FormControl(),
       end: new FormControl()
     });
+    const month: number = new Date().getMonth() + 1;
+    switch (month) {
+      case 1: this.mes = 'Enero';
+        break;
+      case 2: this.mes = 'Febrero';
+        break;
+      case 3: this.mes = 'Marzo';
+        break;
+      case 4: this.mes = 'Abril';
+        break;
+      case 5: this.mes = 'Mayo';
+        break;
+      case 6: this.mes = 'Junio';
+        break;
+      case 7: this.mes = 'Julio';
+        break;
+      case 8: this.mes = 'Agosto';
+        break;
+      case 9: this.mes = 'Septiembre';
+        break;
+      case 10: this.mes = 'Octubre';
+        break;
+      case 11: this.mes = 'Noviembre';
+        break;
+      case 12: this.mes = 'Diciembre';
+        break;
+    };
   }
 
   ngOnInit(): void {
   }
-
 }
