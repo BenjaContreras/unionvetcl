5b831db45a458554caa0fc837ebbb6d9a8c1aa4a
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-14 15:42:06 -0300

    feat(admin/dates): created the routes

--- src/app/admin/modules/dates/dates-routing.module.ts
+++ src/app/admin/modules/dates/dates-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DateScreenComponent } from './screens/date-screen/date-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DateScreenComponent, pathMatch: 'full'},
+  { path: '**', component: DateScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
