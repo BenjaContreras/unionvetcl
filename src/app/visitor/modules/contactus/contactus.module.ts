3335547442773e6029fcaa30fbca652add4dd553
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-19 13:36:07 -0300

    feat(visitor/contact): new components & modules

--- src/app/visitor/modules/contactus/contactus.module.ts
+++ src/app/visitor/modules/contactus/contactus.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ContactusRoutingModule } from './contactus-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { ContactScreenComponent } from './screens/contact-screen/contact-screen.component';
+import { MaterialModule } from '@core/material.module';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 const components: any = [
-  ContactScreenComponent
+  ContactScreenComponent, ContactFormComponent
 ]
 @NgModule({
   declarations: [...components],
   imports: [
     CommonModule,
     ContactusRoutingModule,
-    SharedModule
+    SharedModule,
+    MaterialModule,
   ]
 })
 export class ContactusModule { }
