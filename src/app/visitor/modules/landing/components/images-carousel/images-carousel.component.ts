e833c69a75353fac5ee3b18342f713c73747a954
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-21 02:24:52 -0300

    feat(visitor/landing): carousel added

---
+++ src/app/visitor/modules/landing/components/images-carousel/images-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'images-carousel',
+  templateUrl: './images-carousel.component.html',
+  styleUrls: ['./images-carousel.component.sass']
+})
+export class ImagesCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
