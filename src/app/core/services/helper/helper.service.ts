125679a4c1de2e3548980e36ddc342282b233edf
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-28 02:06:37 -0300

    feat(core/services): helper service include

---
+++ src/app/core/services/helper/helper.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelperService {
+
+  constructor() { }
+
+  public hendleRoutes(route: string): string {
+    if (route === 'Crear cita') {
+      return 'agenda/crear-cita';
+    } else if (route === 'Lista de citas') {
+      return 'agenda/lista-citas';
+    } else if (route === 'Horario') {
+      return 'agenda';
+    } else if (route === 'Crear usuario') {
+      return 'usuarios/crear-usuario';
+    } else if (route === 'Verificar usuario') {
+      return 'usuarios/verificar-usuario';
+    } else if (route === 'Lista de usuarios') {
+      return 'usuarios/lista-usuarios';
+    } else if (route === 'Actualizar productos') {
+      return 'productos/actualizar-productos';
+    } else if (route === 'Crear producto') {
+      return 'productos/crear-producto';
+    } else if (route === 'Lista de productos') {
+      return 'productos/lista-productos';
+    } else if (route === 'Responder consulta') {
+      return 'consultas/responder-consulta';
+    } else if (route === 'Lista de consultas') {
+      return 'consultas/lista-consultas';
+    } else if (route === 'Tips') {
+      return 'publicaciones/tips';
+    } else if (route === 'Crear tip') {
+      return 'publicaciones/tips/crear-tip';
+    } else if (route === 'Actualizar tip') {
+      return 'publicaciones/tips/actualizar-tip';
+    } else if (route === 'Lista de tips') {
+      return 'publicaciones/tips/lista-tips';
+    } else if (route === 'Redes sociales') {
+      return 'publicaciones/redes-sociales';
+    } else if (route === 'Crear publicacion') {
+      return 'publicaciones/tips/crear-publicacion';
+    } else if (route === 'Actualizar publicacion') {
+      return 'publicaciones/tips/actualizar-publicacion';
+    } else if (route === 'Lista de publicaciones') {
+      return 'publicaciones/tips/lista-publicaciones';
+    } else if (route === 'Crear oferta') {
+      return 'ofertas/crear-oferta';
+    } else if (route === 'Lista de ofertas') {
+      return 'ofertas/lista-ofertas';
+    } else if (route === 'Crear contrato') {
+      return 'contratos/crear-contrato';
+    } else if (route === 'Lista de contratos') {
+      return 'contratos/lista-contratos';
+    };
+    return '';
+  };
+}
