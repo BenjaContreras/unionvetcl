d982c8ae41a04a3366464fea2636d6eefdf63989
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-31 19:53:51 -0300

    chore(dates/list): material module included

--- src/app/admin/modules/dates/modules/list-dates/list-dates.module.ts
+++ src/app/admin/modules/dates/modules/list-dates/list-dates.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ListDatesRoutingModule } from './list-dates-routing.module';
 import { ListScreenComponent } from './screens/list-screen/list-screen.component';
 import { ListComponentComponent } from './components/list-component/list-component.component';
+import { MaterialModule } from '@core/material.module';
 
 const components: any = [
   ListScreenComponent,
@@ -14,7 +15,8 @@ const components: any = [
   declarations: [...components],
   imports: [
     CommonModule,
-    ListDatesRoutingModule
+    ListDatesRoutingModule,
+    MaterialModule
   ]
 })
 export class ListDatesModule { }
