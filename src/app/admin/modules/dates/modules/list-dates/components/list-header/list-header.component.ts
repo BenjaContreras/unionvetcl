cd7634447b53a6599aec3322b3e1502ab125764e
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-10-31 17:29:18 -0300

    feat(admin/dates/list): new component for header included

---
+++ src/app/admin/modules/dates/modules/list-dates/components/list-header/list-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'list-header',
+  templateUrl: './list-header.component.html',
+  styleUrls: ['./list-header.component.sass']
+})
+export class ListHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
