e5fde554f86eeba6f17c37a3659a2aa0660ff4b0
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-02 20:43:38 -0300

    chore(admin/users7createuser): variables names changed

--- src/app/admin/modules/users/modules/create-user/components/create-user-form-detail/create-form-detail.component.ts
+++ src/app/admin/modules/users/modules/create-user/components/create-user-form-detail/create-form-detail.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { DatesProviderService } from '@core/providers/dates/dates.service';
-import { Block, HelperService } from '@core/services/helper/helper.service';
+import { HelperService } from '@core/services/helper/helper.service';
 import { NotificationService } from '@core/services/notification/notification.service';
 
 @Component({
@@ -11,25 +10,22 @@ import { NotificationService } from '@core/services/notification/notification.se
 })
 export class CreateFormDetailComponent implements OnInit {
 
-  client: any;
-  @Output() cleanClientEmitter: EventEmitter<any>;
-  public createDateForm: FormGroup;
+  @Output() cleanUserEmitter: EventEmitter<any>;
+  public createUserForm: FormGroup;
   public regionSelected: string;
   public communes: string[];
   public regiones: string[];
-  public blocks: Block[];
   public isLoading: boolean;
 
   constructor(
     private fb: FormBuilder,
     private helperService: HelperService,
     private notificationService: NotificationService,
-    private dateProvider: DatesProviderService,
+    // private userProvider: UserProviderService,
   ) {
-    this.cleanClientEmitter = new EventEmitter<any>();
-    this.client = null;
+    this.cleanUserEmitter = new EventEmitter<any>();
     this.isLoading = false;
-    this.createDateForm = this.fb.group({
+    this.createUserForm = this.fb.group({
       firstName: [null, Validators.required],
       lastName: [null, Validators.required],
       rut: [null, Validators.compose([
@@ -46,10 +42,8 @@ export class CreateFormDetailComponent implements OnInit {
       ],
     });
     this.regionSelected = '';
-    this.blocks = this.helperService.blocks;
     this.communes = [];
     this.regiones = this.helperService.communes.map(commune => commune.name);
-    if (this.client) this.cleanForm();
   }
 
   public setRegionSelected(region: string): void {
@@ -64,15 +58,15 @@ export class CreateFormDetailComponent implements OnInit {
   };
 
   public cleanForm(){
-    for(let data in this.createDateForm.controls) {
-      (<FormControl>this.createDateForm.controls[data]).setValue(null);
-      this.createDateForm.controls[data].setErrors(null);
+    for(let data in this.createUserForm.controls) {
+      (<FormControl>this.createUserForm.controls[data]).setValue(null);
+      this.createUserForm.controls[data].setErrors(null);
     };
     this.regionSelected = '';
   };
 
-  public cleanClient(){
-    this.cleanClientEmitter.emit(null);
+  public emitUser(user: any){
+    this.cleanUserEmitter.emit(user);
   };
 
   myFilter = (d: Date | null): boolean => {
@@ -80,15 +74,15 @@ export class CreateFormDetailComponent implements OnInit {
     return day !== 0 && day !== 6;
   };
 
-  get firstName(): string { return this.createDateForm.get('firstName')?.value };
-  get lastName(): string { return this.createDateForm.get('lastName')?.value };
-  get email(): string { return this.createDateForm.get('email')?.value };
-  get password(): string { return this.createDateForm.get('password')?.value };
-  get address(): string { return this.createDateForm.get('address')?.value };
-  get phone(): string { return this.createDateForm.get('phone')?.value };
-  get rut(): string { return this.createDateForm.get('rut')?.value };
-  get commune(): string { return this.createDateForm.get('commune')?.value }
-  get region(): string { return this.createDateForm.get('region')?.value }
+  get firstName(): string { return this.createUserForm.get('firstName')?.value };
+  get lastName(): string { return this.createUserForm.get('lastName')?.value };
+  get email(): string { return this.createUserForm.get('email')?.value };
+  get password(): string { return this.createUserForm.get('password')?.value };
+  get address(): string { return this.createUserForm.get('address')?.value };
+  get phone(): string { return this.createUserForm.get('phone')?.value };
+  get rut(): string { return this.createUserForm.get('rut')?.value };
+  get commune(): string { return this.createUserForm.get('commune')?.value }
+  get region(): string { return this.createUserForm.get('region')?.value }
 
   async onSubmit(): Promise<any> {
     // if (this.createDateForm.valid){
@@ -113,6 +107,7 @@ export class CreateFormDetailComponent implements OnInit {
     //       try {
     //         this.isLoading = true;
     //         // const result = await this.dateProvider.postDate(newDate).toPromise();
+    //         // this.emitUser(result);
     //         // if (result) this.isLoading = false;
     //         this.notificationService.success(`Se ha creado la cita para el dia ${this.day} en el bloque ${this.block}`);
     //         this.cleanForm();
@@ -130,16 +125,12 @@ export class CreateFormDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (this.client) this.cleanForm();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes.client.currentValue === this.client) this.cleanForm();
-    if (this.client) this.cleanForm();
   }
 
   ngAfterViewInit(): void {
-    if (this.client) this.cleanForm();
   }
 
 }
