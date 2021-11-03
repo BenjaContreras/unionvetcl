986c02ab100e85866dc5d01da7a2397eb82e3286
Hectorveas <hveas10@gmail.com>
2021-11-03 02:10:46 -0300

    feat(products/create): products forms created

---
+++ src/app/admin/modules/products/modules/create-product/components/create-header/create-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-header',
+  templateUrl: './create-header.component.html',
+  styleUrls: ['./create-header.component.sass']
+})
+export class CreateHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
