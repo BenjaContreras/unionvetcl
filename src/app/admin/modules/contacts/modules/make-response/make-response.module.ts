044d6f072099bd0ba9104bdf184b29bac9f77101
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-04 19:50:05 -0300

    feat(admin/contact/response): new components & modules

--- src/app/admin/modules/contacts/modules/make-response/make-response.module.ts
+++ src/app/admin/modules/contacts/modules/make-response/make-response.module.ts
@@ -4,16 +4,24 @@ import { CommonModule } from '@angular/common';
 import { MakeResponseRoutingModule } from './make-response-routing.module';
 import { ResponseScreenComponent } from './screens/response-screen/response-screen.component';
 import { ResponseComponentComponent } from './components/response-component/response-component.component';
+import { MaterialModule } from '@core/material.module';
+import { ResponseHeaderComponent } from './components/response-header/response-header.component';
+import { RutPipe } from '@core/pipes/rut.pipe';
+import { ResponseModalComponent } from './components/response-modal/response-modal.component';
 
 
 @NgModule({
   declarations: [
     ResponseScreenComponent,
-    ResponseComponentComponent
+    ResponseComponentComponent,
+    ResponseHeaderComponent,
+    RutPipe,
+    ResponseModalComponent
   ],
   imports: [
     CommonModule,
-    MakeResponseRoutingModule
+    MakeResponseRoutingModule,
+    MaterialModule,
   ]
 })
 export class MakeResponseModule { }
