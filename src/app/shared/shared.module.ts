08d81e95befe646c61e86cb288c6be95e1c61626
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-04 19:41:29 -0300

    feat(shared): new components included

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ScrollTopButtonComponent } from './components/scroll-top-button/scroll-top-button.component';
 import { LoginComponent } from './components/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RecoverNavComponent } from './components/recover-nav/recover-nav.component';
+import { AlertModalComponent } from './components/alert-modal/alert-modal.component';
 
 const components: any = [
   NavbarComponent, FooterComponent,
   ScrollTopButtonComponent, LoginComponent, 
-  LoginFormComponent
+  LoginFormComponent, RecoverNavComponent, 
+  AlertModalComponent
 ];
 
 @NgModule({
