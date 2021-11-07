e08037a11d436e2ba8fe7faf6b4d334bfa702e73
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-06 22:29:12 -0300

    feat(admin/publications/tips): new components added

---
+++ src/app/admin/modules/publications/modules/tips/components/tip-list/tip-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tip-list',
+  templateUrl: './tip-list.component.html',
+  styleUrls: ['./tip-list.component.sass']
+})
+export class TipListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
