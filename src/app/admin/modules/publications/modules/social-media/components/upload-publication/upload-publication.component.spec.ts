c121a55de84dd6a48ef96959dea26e581a458859
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-06 22:28:42 -0300

    feat(admin/publications/rrss): new components added

---
+++ src/app/admin/modules/publications/modules/social-media/components/upload-publication/upload-publication.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UploadPublicationComponent } from './upload-publication.component';
+
+describe('UploadPublicationComponent', () => {
+  let component: UploadPublicationComponent;
+  let fixture: ComponentFixture<UploadPublicationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UploadPublicationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UploadPublicationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
