b6dd64a745ce10b4c79101afe8a3850e6de63f57
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-08 22:48:17 -0300

    chore(admin/dates): new component & pipe

--- src/app/admin/modules/dates/dates.module.ts
+++ src/app/admin/modules/dates/dates.module.ts
@@ -7,10 +7,13 @@ import { TopCalendarComponent } from './components/top-calendar/top-calendar.com
 import { BottomCalendarComponent } from './components/bottom-calendar/bottom-calendar.component';
 import { MaterialModule } from '@core/material.module';
 import { PatientDetailComponent } from './components/patient-detail/patient-detail.component';
+import { InformationModalComponent } from './components/information-modal/information-modal.component';
+import { StatePipe } from '@core/pipes/state.pipe';
 
 const components: any = [
   DateScreenComponent, TopCalendarComponent,
-  BottomCalendarComponent, PatientDetailComponent
+  BottomCalendarComponent, PatientDetailComponent, 
+  InformationModalComponent, StatePipe
 ]
 
 @NgModule({
