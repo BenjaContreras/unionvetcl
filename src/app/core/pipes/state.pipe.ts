3ad2d909ee009cebe007c010010c43a96ecaf80a
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-08 22:50:08 -0300

    feat(core/pipes): deleted .spect & added a new pipe

---
+++ src/app/core/pipes/state.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'state'
+})
+export class StatePipe implements PipeTransform {
+
+    transform(value: unknown, ...args: unknown[]): unknown {
+        switch (value) {
+            case 1:
+                return 'Pendiente';
+            case 2:
+                return 'Realizada';
+            case 3:
+                return 'Cancelada';
+            default:
+                return null;
+        }
+    }
+}
