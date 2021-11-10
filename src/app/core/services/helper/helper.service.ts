a7ec26eb62a8cf339f56eab830453faec4ebc4d1
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-10 17:46:41 -0300

    feat(core/services/helper): included product list

--- src/app/core/services/helper/helper.service.ts
+++ src/app/core/services/helper/helper.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Product } from '@models/product.model';
 import { rutTools, phoneTools } from 'prettyutils'
 
 @Injectable({
@@ -10,6 +11,7 @@ export class HelperService {
   public blocks: Block[] = BLOCKS;
   public specialCharacters: string[] = SPECIALCHARACTERS;
   public validMails: string[] = VALIDMAILS;
+  public products: Product[] = PRODUCTS;
   public isAdmin: boolean;
 
   constructor() {
@@ -245,3 +247,41 @@ const VALIDMAILS = [
   'gmail.com', 'outlook.com', 'hotmail.com', 'icloud.com', 'yahoo.es', 'yahoo.com', 'mail.pucv.cl', 'sansano.usm.cl', 'codefire.cl',
   'alumnos.uv.cl', 'uv.cl', 'pucv.cl', 'usm.cl', 'uai.cl', 'unab.cl'
 ];
+
+const PRODUCTS = [
+  { brand: 'Microsules', name: 'Vermic total', description: 'Antiparasitario interno de uso oral, para perros y gatos', category: 'Antiparasitario', sale: true},
+  { brand: 'Bayer', name: 'Drontal cat', description: 'Antiparasitario interno de uso oral, para gatos', category: 'Antiparasitario'},
+  { brand: 'Bayer', name: 'Profender: 2,5 a 5 Kg', description: 'Antiparasitario interno de uso tópico (pipeta), para gatos', category: 'Antiparasitario'},
+  { brand: 'Bayer', name: 'Profender: 5 Kg+', description: 'Antiparasitario interno de uso tópico (pipeta), para gatos', category: 'Antiparasitario'},
+  { brand: 'Dragpharma', name: 'Fiprokill', description: 'Antiparasitario externo de uso tópico (spray), para perros y gatos', category: 'Antiparasitario'},
+  { brand: 'Dragpharma', name: 'Fiprokill: Hasta 10 Kg', description: 'Antiparasitario externo de uso tópico (spray), para perros', category: 'Antiparasitario'},
+  { brand: 'Dragpharma', name: 'Fiprokill: Hasta 10 a 20 Kg', description: 'Antiparasitario externo de uso tópico (spray), para perros', category: 'Antiparasitario'},
+  { brand: 'Dragpharma', name: 'Fiprokill: Hasta 20 a 40 Kg', description: 'Antiparasitario externo de uso tópico (spray), para perros', category: 'Antiparasitario'},
+  { brand: 'Dragpharma', name: 'Fiprokill: Hasta 40 a 6 0Kg', description: 'Antiparasitario externo de uso tópico (spray), para perros', category: 'Antiparasitario'},
+  { brand: 'Dragpharma', name: 'Fiprodrag gato', description: 'Antiparasitario externo de uso tópico (spray), para gatos', category: 'Antiparasitario'},
+  { brand: 'Dragpharma', name: 'Naxpet: 10 Mg', description: 'Antiinflamatorio no esteroidal de uso oral (comprimido), para perros y gatos', category: 'Medicamento'},
+  { brand: 'Dragpharma', name: 'Naxpet: 30 Mg', description: 'Antiinflamatorio no esteroidal de uso oral (comprimido), para perros y gatos', category: 'Medicamento'},
+  { brand: 'Dragpharma', name: 'Naxpet: Solución oral', description: 'Antiinflamatorio no esteroidal de uso oral (jarabe), para perros y gatos', category: 'Medicamento'},
+  { brand: 'Dragpharma', name: 'Apeticat', description: 'Suplemento vitamínico de uso oral (jarabe), para gatos', category: 'Suplemento'},
+  { brand: 'Dragpharma', name: 'Doguivit: cachorro', description: 'Suplemento vitamínico de uso oral (jarabe), para perros', category: 'Suplemento'},
+  { brand: 'Dragpharma', name: 'Doguivit: adulto', description: 'Suplemento vitamínico de uso oral (jarabe), para perros', category: 'Suplemento'},
+  { brand: 'Dragpharma', name: 'Doguivit: senior', description: 'Suplemento vitamínico de uso oral (jarabe), para perros', category: 'Suplemento'},
+  { brand: 'Dragpharma', name: 'Inmunopet', description: 'Aditivo inmunoestimulante de uso oral (jarave), para perros y gatos', category: 'Medicamento'},
+  { brand: 'Dragpharma', name: 'Papainpet', description: 'Suplemento nutricional de uso oral (comprimido), para perros y gatos', category: 'Suplemento'},
+  { brand: 'Dragpharma', name: 'Pacifor', description: 'Tranquilizante de uso oral (gotas), para perros y gatos', category: 'Medicamento'},
+  { brand: 'Dragpharma', name: 'Ultrafil plus', description: 'Antibiótico, Antimicótico, Antiinflamatorio y Anti ácaros de uso tópico (solución ótica), para perros y gatos', category: 'Medicamento'},
+  { brand: 'Dragpharma', name: 'Doximicion: solución oral', description: 'Antibiótico de uso oral (jarabe), para perros y gatos', category: 'Medicamento'},
+  { brand: 'Frontline Labs', name: 'Nexgard: 2 a 4 Kg', description: 'Antiparasitario externo de uso oral (comprimido palatable), para perros', category: 'Antiparasitario'},
+  { brand: 'Frontline Labs', name: 'Nexgard: 4,1 a 10 Kg', description: 'Antiparasitario externo de uso oral (comprimido palatable), para perros', category: 'Antiparasitario'},
+  { brand: 'Frontline Labs', name: 'Nexgard: 10,1 a 25 Kg', description: 'Antiparasitario externo de uso oral (comprimido palatable), para perros', category: 'Antiparasitario'},
+  { brand: 'Frontline Labs', name: 'Nexgard: 25,1 a 50 Kg', description: 'Antiparasitario externo de uso oral (comprimido palatable), para perros', category: 'Antiparasitario'},
+  { brand: 'Traper', name: 'Shampoo: gato', description: 'Shampo especial para gatos', category: 'Shampoo'},
+  { brand: 'Traper', name: 'Shampoo: pelaje claro', description: 'Shampo especial para perros con pelaje claro, resaltando el color de este', category: 'Shampoo'},
+  { brand: 'Traper', name: 'Shampoo: aloe vera', description: 'Shampo especial para perros con loción de aloe vera para brindarles mayor frescura', category: 'Shampoo'},
+  { brand: 'Traper', name: 'Shampoo: neutro', description: 'Shampo para que cualquier animal luzca su radiante y bello pelaje', category: 'Shampoo'},
+  { brand: 'Traper', name: 'Shampoo: cachorro', description: 'Shampo especial para cachorros, evitando la irritación y malestar en ellos', category: 'Shampoo'},
+  { brand: 'Dragpharma', name: 'Sir Dog: pelaje negro', description: 'Shampo especial para aquellos perros con pelaje oscuro', category: 'Shampoo'},
+  { brand: 'Dragpharma', name: 'Sir Dog: pelaje blanco', description: 'Shampo especial para aquellos perros con pelaje blanco', category: 'Shampoo'},
+  { brand: 'Dragpharma', name: 'Tidy: gato', description: 'Shampo en seco, especial para gatos', category: 'Shampoo'},
+  { brand: 'Dragpharma', name: 'Tidy: perro', description: 'Shampo en seco, especial para perros', category: 'Shampoo'},
+]
