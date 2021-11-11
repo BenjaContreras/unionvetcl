2519a217ee075d4e82124ebac694681531ce3058
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-11 01:50:26 -0300

    feat(admin/contacts/list): included API info in MatTable

--- src/app/admin/modules/contacts/modules/contacts-list/components/list-component/list-component.component.ts
+++ src/app/admin/modules/contacts/modules/contacts-list/components/list-component/list-component.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, ViewChild, AfterViewInit, AfterContentChecked } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { ContactProviderService } from '@core/providers/contacts/contact-provider.service';
+import { Contact } from '@models/contact.model';
 import { ModalComponent } from '../modal/modal.component';
 
 @Component({
@@ -10,36 +12,48 @@ import { ModalComponent } from '../modal/modal.component';
   templateUrl: './list-component.component.html',
   styleUrls: ['./list-component.component.sass']
 })
-export class ListComponentComponent implements OnInit {
+export class ListComponentComponent implements OnInit, OnChanges, AfterViewInit {
 
-  public displayedColumns: string[] = ['name', 'rut', 'address', 'animal', 'fecha'];
-  public dataSource: MatTableDataSource<any>;
-  public expandedElement: any;
+  public displayedColumns: string[] = ['fullName', 'email', 'phone', 'date', 'message'];
+  public dataSource: MatTableDataSource<any> = new MatTableDataSource();
   public contactSelected: any;
+  public contacts: Contact[];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private dialog: MatDialog) {
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-    this.expandedElement = null;
+  constructor(
+    private dialog: MatDialog,
+    private contactP: ContactProviderService
+  ) {
+    this.contacts = [];
     this.contactSelected = null;
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    await this.setMatTable();
   }
-  
+
+  private async setMatTable(): Promise<void> {
+    const result = await this.contactP.getAllContacts().toPromise();
+    if (result){
+      this.contacts = result;
+      this.dataSource = new MatTableDataSource(this.contacts);
+      this.dataSource.paginator = this.paginator;
+      this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
+      this.dataSource.sort = this.sort;
+    }
+  };
+
   ngOnChanges() {
-    this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     this.dataSource.paginator = this.paginator;
+    this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
     this.dataSource.sort = this.sort;
   }
 
   ngAfterViewInit() {
-    this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     this.dataSource.paginator = this.paginator;
+    this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
     this.dataSource.sort = this.sort;
   }
 
@@ -49,43 +63,14 @@ export class ListComponentComponent implements OnInit {
   };
 
   public openModal(event: string){
-    if (event === 'edit') {
-      this.dialog.open(ModalComponent, {
-        width: '700px',
-        data: {
-          appointment: this.contactSelected,
-          type: 'edit',
-        }
-      }).afterClosed().subscribe(result => {
-        this.contactSelected = null;
-      });
-    } else {
-      this.dialog.open(ModalComponent, {
-        width: '700px',
-        data: {
-          appointment: this.contactSelected,
-          type: 'delete',
-        }
-      }).afterClosed().subscribe(result => {
-        this.contactSelected = null;
-      });
-    };
+    this.dialog.open(ModalComponent, {
+      width: '700px',
+      data: {
+        appointment: this.contactSelected,
+        type: event,
+      }
+    }).afterClosed().subscribe(result => {
+      this.contactSelected = null;
+    });
   }
 }
-
-const ELEMENT_DATA: {name: string, rut: string, address: string, animal: string, fecha: string}[] = [
-  { name: 'Juan Jose', rut: '20.360.152-9', address: 'Federico Araneda, 1809', animal: 'Kira', fecha: '10 de Junio, 2021'},
-  { name: 'Pedro Carcuro', rut: '20.370.152-9', address: 'Federico Estrada, 1839', animal: 'Cuchurrumin', fecha: '10 de Julio, 2018'},
-  { name: 'Jose de la Masa', rut: '5.370.522-7', address: 'Eleuterio ramirez, 1839', animal: 'Casper', fecha: '5 de Marzo, 2020'},
-  { name: 'Patricio Knopeln', rut: '9.370.365-9', address: 'Eleuterio ramirez, 929', animal: 'Rocky', fecha: '15 de Abril, 2017'},
-  { name: 'Eduardo Araneda', rut: '19.002.987-1', address: 'Comercio, 997', animal: 'Bobby', fecha: '14 de Mayo, 2020'},
-  { name: 'Joel Lespai', rut: '12.702.982-K', address: 'Pje Alcantara, 95', animal: 'Pedrito', fecha: '30 de Agosto, 2021'},
-  { name: 'Guillermo Oporto', rut: '13.123.951-8', address: 'Pje Alcantara, 99', animal: 'Chimuelo', fecha: '4 de Septiembre, 2020'},
-  { name: 'Juan Jose', rut: '20.360.152-9', address: 'Federico Araneda, 1809', animal: 'Kira', fecha: '10 de Junio, 2021'},
-  { name: 'Pedro Carcuro', rut: '20.370.152-9', address: 'Federico Estrada, 1839', animal: 'Cuchurrumin', fecha: '10 de Julio, 2018'},
-  { name: 'Jose de la Masa', rut: '5.370.522-7', address: 'Eleuterio ramirez, 1839', animal: 'Casper', fecha: '5 de Marzo, 2020'},
-  { name: 'Patricio Knopeln', rut: '9.370.365-9', address: 'Eleuterio ramirez, 929', animal: 'Rocky', fecha: '15 de Abril, 2017'},
-  { name: 'Eduardo Araneda', rut: '19.002.987-1', address: 'Comercio, 997', animal: 'Bobby', fecha: '14 de Mayo, 2020'},
-  { name: 'Joel Lespai', rut: '12.702.982-K', address: 'Pje Alcantara, 95', animal: 'Pedrito', fecha: '30 de Agosto, 2021'},
-  { name: 'Guillermo Oporto', rut: '13.123.951-8', address: 'Pje Alcantara, 99', animal: 'Chimuelo', fecha: '4 de Septiembre, 2020'},
-];
