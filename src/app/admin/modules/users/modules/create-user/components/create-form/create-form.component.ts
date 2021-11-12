f67bcbbb6e03a6202d259442874a2fad4fda38c8
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-12 04:49:18 -0300

    feat(admin/users/create): added functions to make the search works 100%

--- src/app/admin/modules/users/modules/create-user/components/create-form/create-form.component.ts
+++ src/app/admin/modules/users/modules/create-user/components/create-form/create-form.component.ts
@@ -1,37 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSelect } from '@angular/material/select';
+import { ReplaySubject, Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'create-form',
   templateUrl: './create-form.component.html',
   styleUrls: ['./create-form.component.sass']
 })
-export class CreateFormComponent implements OnInit {
+export class CreateFormComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  public clientSelectionForm: FormGroup;
   public selectedClient: any;
   public clients: any[];
+  public ownerFrmCtrl: FormControl = new FormControl(null);
+  public ownerFrmFilterCtrl: FormControl = new FormControl(null);
+  public filteredClients: ReplaySubject<string[]> = new ReplaySubject<string[]>(1);
+  protected onDestroy = new Subject<void>();
 
-  constructor(private fb: FormBuilder) { 
-    this.clientSelectionForm = this.fb.group({
-      client: [null, Validators.required],
-    });
+  @ViewChild('singleSelect') singleSelect!: MatSelect;
+
+  constructor() {
     this.selectedClient = null;
     this.clients = ['Jose', 'Pedro', 'Juan', 'Roberto', 'Alexis'];
   }
 
   ngOnInit(): void {
+    this.ownerFrmCtrl.setValue(this.selectedClient);
+    this.filteredClients.next(this.clients.slice());
+    this.ownerFrmFilterCtrl.valueChanges
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe(() => {
+        this.filterClients();
+      });
   }
 
+  ngAfterViewInit(): void {
+    this.setInitialValue();
+  };
+
+  protected setInitialValue(): void {
+    this.filteredClients
+    .pipe(take(1), takeUntil(this.onDestroy)).subscribe(() => {
+      this.singleSelect.compareWith = (a: any, b: any) => a && b && a === b;
+    });
+  };
+
+  protected filterClients(): void {
+    if (!this.clients) return;
+    let search = this.ownerFrmFilterCtrl.value;
+    if (!search) {
+      this.filteredClients.next(this.clients.slice());
+      return;
+    } else search = search.toLowerCase();
+    this.filteredClients.next(
+      this.clients.filter(client => client.toLowerCase().includes(search))
+    );
+  };
+
+
   public setSelectedClient(client: any): void {
-    this.selectedClient = client;
+    this.selectedClient = client.value;
   };
 
   public emitterReciver(event: any): void {
-    for(let data in this.clientSelectionForm.controls) {
-      (<FormControl>this.clientSelectionForm.controls[data]).setValue(null);
-      this.clientSelectionForm.controls[data].setErrors(null);
-    };
     this.selectedClient = event;
+    this.ownerFrmCtrl.setValue(this.selectedClient);
+  };
+
+  ngOnDestroy(): void {
+    this.onDestroy.next();
+    this.onDestroy.complete();
   };
 }
