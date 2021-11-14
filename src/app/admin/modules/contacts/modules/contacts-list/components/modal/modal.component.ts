eb120d1dcefaab70ac67580afa0f46f851b98796
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-14 04:59:57 -0300

    feat(admin/contact/list): modal fixed

--- src/app/admin/modules/contacts/modules/contacts-list/components/modal/modal.component.ts
+++ src/app/admin/modules/contacts/modules/contacts-list/components/modal/modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ContactProviderService } from '@core/providers/contacts/contact-provider.service';
+import { NotificationService } from '@core/services/notification/notification.service';
+import { Contact } from '@models/contact.model';
 
 @Component({
   selector: 'app-modal',
@@ -9,53 +12,70 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ModalComponent implements OnInit {
 
-  public date: any;
+  public contact: Contact | null;
   public event: string;
-  public editDateForm: FormGroup;
-  public stateOptions: {type: string, value: number}[];
+  public editContactForm: FormGroup;
   public isLoading: boolean;
 
   constructor(
     private dialogRef: MatDialogRef<ModalComponent>,
     private fb: FormBuilder,
-    @Inject(MAT_DIALOG_DATA) data: { appointment: any, type: string }
+    private notifications: NotificationService,
+    private contactP: ContactProviderService,
+    @Inject(MAT_DIALOG_DATA) data: { contact: any, type: string }
   ) { 
-    if (data.appointment) {
-      this.date = data.appointment;
-    };
+    if (data.contact) this.contact = data.contact;
+    else this.contact = null;
     this.isLoading = false;
     this.event = data.type;
-    this.editDateForm = this.fb.group({
-      nameOwner: [this.date.name],
-      rutOwner: [this.date.rut],
-      address: [this.date.address],
-      petName: [this.date.animal],
-      motion: [null],
-      state: [null, Validators.required],
+    this.editContactForm = this.fb.group({
+      name: [this.contact?.fullName],
+      phone: [this.contact?.phone],
+      email: [this.contact?.email],
+      message: [this.contact?.message, Validators.maxLength(400)],
+      response: [this.contact?.response ? this.contact.response : null, Validators.maxLength(400)],
+      isReaded: [this.contact?.isReaded]
     });
-    this.stateOptions = [
-      { type: 'Pendiente', value: 1 },
-      { type: 'Realizada', value: 2 },
-      { type: 'Cancelada', value: 3 },
-    ];
   }
 
   ngOnInit(): void {
   }
 
   public cleanForm(){
-    for(let data in this.editDateForm.controls) {
-      (<FormControl>this.editDateForm.controls[data]).setValue(null);
-      this.editDateForm.controls[data].setErrors(null);
+    for(let data in this.editContactForm.controls) {
+      (<FormControl>this.editContactForm.controls[data]).setValue(null);
+      this.editContactForm.controls[data].setErrors(null);
     };
   };
 
-  public updateContact(message: boolean) {
+  public async updateContact() {
     this.isLoading = true;
-    if (message){
-      //
+    if (this.editContactForm.valid) {
+      try {
+        let contact: Partial<Contact> = {
+          fullName: this.editContactForm.value.name,
+          phone: this.editContactForm.value.phone,
+          email: this.editContactForm.value.email,
+          message: this.editContactForm.value.message,
+          response: this.editContactForm.value.response,
+          isReaded: this.editContactForm.value.isReaded,
+          updatedAt: new Date()
+        };
+        const result = await this.contactP.updateContact(this.contact?._id!, contact).toPromise();
+        if (result) this.isLoading = false;
+        this.notifications.success('Solicitud actualizada con éxito!');
+        this.cleanForm();
+        this.dialogRef.close();
+      } catch (e) {
+        console.log(e);
+        this.notifications.error('No pudimos actualizar la solicitud, intente otra vez');
+        this.isLoading = false;
+        this.cleanForm();
+      }
     } else {
-      //
-    };
+      this.notifications.error('Algo ocurrió con el formulario, pruebe otra vez');
+      this.isLoading = false;
+      this.cleanForm();
+    }
   };
 }
