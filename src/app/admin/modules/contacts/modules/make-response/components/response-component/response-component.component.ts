44060cd555e930b3717635f1589bf7ddcf94d751
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-14 05:00:51 -0300

    refactor(admin/contacts/response): response component fixed

--- src/app/admin/modules/contacts/modules/make-response/components/response-component/response-component.component.ts
+++ src/app/admin/modules/contacts/modules/make-response/components/response-component/response-component.component.ts
@@ -1,73 +1,76 @@
 import { ResponseModalComponent } from './../response-modal/response-modal.component';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Contact } from '@models/contact.model';
+import { ContactProviderService } from '@core/providers/contacts/contact-provider.service';
+import { HelperService } from '@core/services/helper/helper.service';
 
 @Component({
   selector: 'response-component',
   templateUrl: './response-component.component.html',
   styleUrls: ['./response-component.component.sass']
 })
-export class ResponseComponentComponent implements OnInit {
+export class ResponseComponentComponent implements OnInit, AfterViewInit, OnChanges {
 
-  public displayedColumns: string[] = ['name', 'rut', 'address', 'animal', 'fecha'];
-  public dataSource: MatTableDataSource<any>;
-  public expandedElement: any;
+  public displayedColumns: string[] = ['fullName', 'email', 'phone', 'date', 'message'];
+  public dataSource: MatTableDataSource<any> = new MatTableDataSource();
   public clicked: boolean;
-  public elementSelected: any;
+  public contactSelected: Contact | null;
+  public contacts: Contact[];
+  public existsPendingContacts: boolean;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private dialog: MatDialog) {
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-    this.expandedElement = null;
+  constructor(
+    private dialog: MatDialog,
+    private contactP: ContactProviderService,
+    private helper: HelperService
+  ) {
+    this.contactSelected = null;
     this.clicked = false;
+    this.contacts = [];
+    this.existsPendingContacts = true;
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    await this.setMatTable();
   }
-  
+
+  private async setMatTable(): Promise<void> {
+    const result = await this.contactP.getAllContacts().toPromise();
+    if (result){
+      this.contacts = result.filter(contact => contact.isReaded === false);
+      if (this.contacts.length === 0) this.existsPendingContacts = false;
+      this.dataSource = new MatTableDataSource(this.contacts);
+      this.dataSource.paginator = this.paginator;
+      this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
+    }
+  };
+
   ngOnChanges() {
-    this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
   }
 
   ngAfterViewInit() {
-    this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Contactos a mostrar: ';
   }
 
   public openModal(element: any) {
     this.clicked = true;
-    this.elementSelected = element;
+    this.contactSelected = element;
     this.dialog.open(ResponseModalComponent, {
-      //
+      width: '600px',
+      data: {
+        contact: this.contactSelected
+      }
     }).afterClosed().subscribe(result => {
-        this.elementSelected = null;
+        this.contactSelected = null;
+        this.setMatTable();
     });
   }
 }
-
-const ELEMENT_DATA: {name: string, rut: number, address: string, animal: string, fecha: string}[] = [
-  { name: 'Juan Jose', rut: 203601529, address: 'Federico Araneda, 1809', animal: 'Kira', fecha: '10 de Junio, 2021'},
-  { name: 'Pedro Carcuro', rut: 203701529, address: 'Federico Estrada, 1839', animal: 'Cuchurrumin', fecha: '10 de Julio, 2018'},
-  { name: 'Jose de la Masa', rut: 53705227, address: 'Eleuterio ramirez, 1839', animal: 'Casper', fecha: '5 de Marzo, 2020'},
-  { name: 'Patricio Knopeln', rut: 93703659, address: 'Eleuterio ramirez, 929', animal: 'Rocky', fecha: '15 de Abril, 2017'},
-  { name: 'Eduardo Araneda', rut: 190029871, address: 'Comercio, 997', animal: 'Bobby', fecha: '14 de Mayo, 2020'},
-  { name: 'Joel Lespai', rut: 127029822, address: 'Pje Alcantara, 95', animal: 'Pedrito', fecha: '30 de Agosto, 2021'},
-  { name: 'Guillermo Oporto', rut: 131239518, address: 'Pje Alcantara, 99', animal: 'Chimuelo', fecha: '4 de Septiembre, 2020'},
-  { name: 'Juan Jose', rut: 203601259, address: 'Federico Araneda, 1809', animal: 'Kira', fecha: '10 de Junio, 2021'},
-  { name: 'Pedro Carcuro', rut: 203701529, address: 'Federico Estrada, 1839', animal: 'Cuchurrumin', fecha: '10 de Julio, 2018'},
-  { name: 'Jose de la Masa', rut: 53705227, address: 'Eleuterio ramirez, 1839', animal: 'Casper', fecha: '5 de Marzo, 2020'},
-  { name: 'Patricio Knopeln', rut: 93703659, address: 'Eleuterio ramirez, 929', animal: 'Rocky', fecha: '15 de Abril, 2017'},
-  { name: 'Eduardo Araneda', rut: 190029871, address: 'Comercio, 997', animal: 'Bobby', fecha: '14 de Mayo, 2020'},
-  { name: 'Joel Lespai', rut: 12702978, address: 'Pje Alcantara, 95', animal: 'Pedrito', fecha: '30 de Agosto, 2021'},
-  { name: 'Guillermo Oporto', rut: 131239518, address: 'Pje Alcantara, 99', animal: 'Chimuelo', fecha: '4 de Septiembre, 2020'},
-];
