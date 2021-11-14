ab27a53397dd87c6328299956f4e4dc29e056c1f
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-14 05:06:41 -0300

    feat(admin/products/create): ngx-mat added

--- src/app/admin/modules/products/modules/create-product/components/create-form-detail/create-form-detail.component.ts
+++ src/app/admin/modules/products/modules/create-product/components/create-form-detail/create-form-detail.component.ts
@@ -1,26 +1,41 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSelect } from '@angular/material/select';
 import { ProductProviderService } from '@core/providers/products/product-provider.service';
 import { Block, HelperService } from '@core/services/helper/helper.service';
 import { NotificationService } from '@core/services/notification/notification.service';
 import { Product } from '@models/product.model';
+import { ReplaySubject, Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-form-detail',
   templateUrl: './create-form-detail.component.html',
   styleUrls: ['./create-form-detail.component.sass']
 })
-export class CreateFormDetailComponent implements OnInit {
+export class CreateFormDetailComponent implements OnInit, AfterViewInit, OnChanges {
 
   client: any;
   public createProductForm: FormGroup;
   public isLoading: boolean;
+  public categoryFrmCtrl: FormControl = new FormControl(null, Validators.required);
+  public categoryFrmFilterCtrl: FormControl = new FormControl(null);
+  public filteredCategories: ReplaySubject<string[]> = new ReplaySubject<string[]>(1);
+  protected onDestroy = new Subject<void>();
+  public categories: string[];
+  public newCategory: boolean;
+  public validForm: boolean;
+
+  @ViewChild('singleSelect') singleSelect!: MatSelect;
 
   constructor(
     private fb: FormBuilder,
     private notificationService: NotificationService,
     private productProvider: ProductProviderService,
+    private helper: HelperService
   ) {
+    this.validForm = true;
+    this.newCategory = false;
     this.isLoading = false;
     this.createProductForm = this.fb.group({
       imageUrl: [""],
@@ -31,13 +46,18 @@ export class CreateFormDetailComponent implements OnInit {
       sale: [false],
       category: [null, Validators.required],
     });
+    this.categories = [];
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+  };
 
   public cleanForm(){
     for(let data in this.createProductForm.controls) {
       (<FormControl>this.createProductForm.controls[data]).setValue(null);
       this.createProductForm.controls[data].setErrors(null);
     };
+    this.validForm = false;
   };
 
   myFilter = (d: Date | null): boolean => {
@@ -70,22 +90,52 @@ export class CreateFormDetailComponent implements OnInit {
         this.notificationService.success(`Se ha creado el producto con exito!`);
         this.cleanForm();
       } catch (e) {
+        this.isLoading = false;
         console.log(e);
         this.notificationService.error('No se pudo realizar tu solicitud, intente otra vez');
       }
     };
   }
 
-  ngOnInit(): void {
-
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-
+  async ngOnInit(): Promise<void> {
+    this.categories = await this.helper.getCategories();
+    this.categoryFrmCtrl.setValue(null);
+    this.filteredCategories.next(this.categories.slice());
+    this.categoryFrmFilterCtrl.valueChanges
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe(() => {
+        this.filterCategories();
+      });
   }
 
   ngAfterViewInit(): void {
+    this.setInitialValue();
+  };
 
-  }
+  public setSelectedCategory(category: any): void {
+    if (category.value === 'Crear categoria'){ 
+      this.newCategory = true;
+      this.createProductForm.controls['category'].setValue(null);
+      this.createProductForm.controls['category'].setErrors(null);
+    } else this.createProductForm.controls['category'].setValue(category.value);
+  };
+  
+  protected setInitialValue(): void {
+    this.filteredCategories
+    .pipe(take(1), takeUntil(this.onDestroy)).subscribe(() => {
+      this.singleSelect.compareWith = (a: any, b: any) => a && b && a === b;
+    });
+  };
 
+  protected filterCategories(): void {
+    if (!this.categories) return;
+    let search = this.categoryFrmFilterCtrl.value;
+    if (!search) {
+      this.filteredCategories.next(this.categories.slice());
+      return;
+    } else search = search.toLowerCase();
+    this.filteredCategories.next(
+      this.categories.filter(category => category.toLowerCase().includes(search))
+    );
+  };
 }
