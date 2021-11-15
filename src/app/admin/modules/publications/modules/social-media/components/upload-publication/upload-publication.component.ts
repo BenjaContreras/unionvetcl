51fdb8c57e760efc024339cd7a86b5cc882531fa
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-15 01:46:35 -0300

    feat(admin/rrss/upload-link): component added

--- src/app/admin/modules/publications/modules/social-media/components/upload-publication/upload-publication.component.ts
+++ src/app/admin/modules/publications/modules/social-media/components/upload-publication/upload-publication.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { PublicationProviderService } from '@core/providers/publications/publication-provider.service';
+import { HelperService } from '@core/services/helper/helper.service';
+import { NotificationService } from '@core/services/notification/notification.service';
 
 @Component({
   selector: 'app-upload-publication',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadPublicationComponent implements OnInit {
 
-  constructor() { }
+  public link: string;
+  public isLoading: boolean;
+
+  constructor(
+    private dialogRef: MatDialogRef<UploadPublicationComponent>,
+    private notifications: NotificationService,
+    private helper: HelperService,
+    private rrss: PublicationProviderService
+  ) {
+    this.link = '';
+    this.isLoading = false;
+  }
 
   ngOnInit(): void {
   }
 
+  public async uploadLink(): Promise<void> {
+    this.isLoading = true;
+    this.link.concat('/embed');
+    if (this.link?.length){
+      if (this.helper.verifyLink(this.link).verify){
+        try {
+          const result = await this.rrss.postPublication(this.link).toPromise();
+          if (result) this.isLoading = false;
+          this.notifications.success('Publicación subida correctamente!');
+          this.dialogRef.close();
+          this.link = '';
+        } catch (e) {
+          console.log(e);
+          this.isLoading = false;
+          this.notifications.error('Error al subir el link, intente otra vez');
+        }
+      } else this.notifications.error('Verifique bien el link, contiene caracteres invalidos');
+    };
+  };
 }
