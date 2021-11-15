b17e4dea89585e28ba9719acf9d87537183a615d
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-15 01:48:09 -0300

    feat(core/models): date model fixed

--- src/app/core/models/date.models.ts
+++ src/app/core/models/date.models.ts
@@ -1,9 +1,14 @@
 import { User } from "./user.models";
 
+export interface Time {
+    day: number;
+    month: number;
+    year: number;
+};
 export interface DateModel {
     _id?: string,
-    day: string,
-    block: string,
+    date: Time,
+    block: number,
     user: User,
     createdAt?: Date,
     updatedAt?: Date
