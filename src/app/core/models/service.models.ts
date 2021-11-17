6e69c57b1d2df636f0aeef7f9d8b2b77b1458a6c
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-17 03:08:04 -0300

    feat(core7modesl): service images included

--- src/app/core/models/service.models.ts
+++ src/app/core/models/service.models.ts
@@ -1,8 +1,11 @@
 export interface Service {
-  id?: string,
+  _id?: string,
   name: string,
   description: string[],
-  imagesURL: string[],
+  imageOne?: string,
+  imageTwo?: string,
+  imageThree?: string,
+  imageFour?: string,
   createdAt?: Date,
   updatedAt?: Date
 };
