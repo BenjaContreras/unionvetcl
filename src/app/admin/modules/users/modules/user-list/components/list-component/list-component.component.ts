afb8ab3a66b051d4e2f82ea0433111d0ed0e0585
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-11-18 22:18:44 -0300

    feat(admin/users/list): use of api +

--- src/app/admin/modules/users/modules/user-list/components/list-component/list-component.component.ts
+++ src/app/admin/modules/users/modules/user-list/components/list-component/list-component.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { UserProviderService } from '@core/providers/user/user-provider.service';
+import { User } from '@models/user.models';
 import { ModalComponent } from '../modal/modal.component';
 
 @Component({
@@ -13,34 +15,44 @@ import { ModalComponent } from '../modal/modal.component';
 export class ListComponentComponent implements OnInit {
 
   public displayedColumns: string[] = ['name', 'rut', 'address', 'animal'];
-  public dataSource: MatTableDataSource<any>;
+  public dataSource: MatTableDataSource<any> = new MatTableDataSource();
   public expandedElement: any;
-  public userSelected: any;
+  public userSelected: User | null;
+  public users: User[];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private dialog: MatDialog) {
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+  constructor(
+    private dialog: MatDialog,
+    private userP: UserProviderService
+  ) {
     this.expandedElement = null;
     this.userSelected = null;
+    this.users = [];
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    await this.setMatTable();
   }
+
+  private async setMatTable(): Promise<void> {
+    const result = await this.userP.getAllUsers().toPromise();
+    if (result){
+      this.users = result;
+      this.dataSource = new MatTableDataSource(this.users);
+      this.dataSource.paginator = this.paginator;
+      this.paginator._intl.itemsPerPageLabel = 'Usuarios a mostrar: ';
+    }
+  };
 
   ngOnChanges() {
-    this.paginator._intl.itemsPerPageLabel = 'Usuarios a mostrar: ';
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Usuarios a mostrar: ';
   }
 
   ngAfterViewInit() {
-    this.paginator._intl.itemsPerPageLabel = 'Usuarios a mostrar: ';
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Usuarios a mostrar: ';
   }
 
   onKey(event: Event){
@@ -48,11 +60,11 @@ export class ListComponentComponent implements OnInit {
     this.dataSource.filter = filterValue?.trim().toLowerCase();
   };
 
-  public openModal(event: string){
+  public openModal(event: string) {
     this.dialog.open(ModalComponent, {
       width: '700px',
       data: {
-        appointment: this.userSelected,
+        user: this.userSelected,
         type: event,
       }
     }).afterClosed().subscribe(result => {
@@ -61,20 +73,3 @@ export class ListComponentComponent implements OnInit {
   }
 
 }
-
-const ELEMENT_DATA: {name: string, rut: string, address: string, animal: string}[] = [
-  { name: 'Juan Jose', rut: '20.360.152-9', address: 'Federico Araneda, 1809', animal: 'Kira'},
-  { name: 'Pedro Carcuro', rut: '20.370.152-9', address: 'Federico Estrada, 1839', animal: 'Cuchurrumin'},
-  { name: 'Jose de la Masa', rut: '5.370.522-7', address: 'Eleuterio ramirez, 1839', animal: 'Casper'},
-  { name: 'Patricio Knopeln', rut: '9.370.365-9', address: 'Eleuterio ramirez, 929', animal: 'Rocky'},
-  { name: 'Eduardo Araneda', rut: '19.002.987-1', address: 'Comercio, 997', animal: 'Bobby'},
-  { name: 'Joel Lespai', rut: '12.702.982-K', address: 'Pje Alcantara, 95', animal: 'Pedrito'},
-  { name: 'Guillermo Oporto', rut: '13.123.951-8', address: 'Pje Alcantara, 99', animal: 'Chimuelo'},
-  { name: 'Juan Jose', rut: '20.360.152-9', address: 'Federico Araneda, 1809', animal: 'Kira'},
-  { name: 'Pedro Carcuro', rut: '20.370.152-9', address: 'Federico Estrada, 1839', animal: 'Cuchurrumin'},
-  { name: 'Jose de la Masa', rut: '5.370.522-7', address: 'Eleuterio ramirez, 1839', animal: 'Casper'},
-  { name: 'Patricio Knopeln', rut: '9.370.365-9', address: 'Eleuterio ramirez, 929', animal: 'Rocky'},
-  { name: 'Eduardo Araneda', rut: '19.002.987-1', address: 'Comercio, 997', animal: 'Bobby'},
-  { name: 'Joel Lespai', rut: '12.702.982-K', address: 'Pje Alcantara, 95', animal: 'Pedrito'},
-  { name: 'Guillermo Oporto', rut: '13.123.951-8', address: 'Pje Alcantara, 99', animal: 'Chimuelo'},
-];
